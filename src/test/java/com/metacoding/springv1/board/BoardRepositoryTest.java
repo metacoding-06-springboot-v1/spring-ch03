777cd57324c744fe320d1cdbaf43f7864a2b0457
metacoding <getinthere@naver.com>
2026-01-28 15:53:31 +0900

    flush 추가

--- src/test/java/com/metacoding/springv1/board/BoardRepositoryTest.java
+++ src/test/java/com/metacoding/springv1/board/BoardRepositoryTest.java
@@ -11,47 +11,47 @@ import jakarta.persistence.EntityManager;
 @DataJpaTest
 public class BoardRepositoryTest {
 
-    @Autowired 
+    @Autowired
     private BoardRepository boardRepository;
 
     @Autowired
     private EntityManager em;
 
     @Test
-    public void findById_test(){
-            //given
-            int id = 2 ;
-            //when
-            Board board = boardRepository.findById(id).get();
-            //eye
-            System.out.println("=======================");
-            System.out.println("Board Title : " + board.getTitle());
-            System.out.println("Board Content : " + board.getContent());
+    public void findById_test() {
+        // given
+        int id = 2;
+        // when
+        Board board = boardRepository.findById(id).get();
+        // eye
+        System.out.println("=======================");
+        System.out.println("Board Title : " + board.getTitle());
+        System.out.println("Board Content : " + board.getContent());
     }
 
     @Test
-    public void findAll_test(){
-        //given
-        
-        //when
-        List<Board> boards = boardRepository.findAll();       
-        //eye
+    public void findAll_test() {
+        // given
+
+        // when
+        List<Board> boards = boardRepository.findAll();
+        // eye
         System.out.println("=======================");
         System.out.println("Board Count : " + boards.size());
-        System.out.println("Board 1 title :" +boards.get(0).getTitle());
-        System.out.println("Board 2 content :" +boards.get(1).getContent());
+        System.out.println("Board 1 title :" + boards.get(0).getTitle());
+        System.out.println("Board 2 content :" + boards.get(1).getContent());
     }
 
     @Test
-    public void save_test(){
-        //given
+    public void save_test() {
+        // given
         Board board = Board.builder()
-            .title("title3")
-            .content("content3")
-            .build();
-        //when
+                .title("title3")
+                .content("content3")
+                .build();
+        // when
         boardRepository.save(board);
-        //eye
+        // eye
         List<Board> boards = boardRepository.findAll();
         System.out.println("=======================");
         System.out.println("Board Count : " + boards.size());
@@ -59,17 +59,17 @@ public class BoardRepositoryTest {
         System.out.println("Board Title: " + boards.get(2).getTitle());
         System.out.println("Board Content: " + boards.get(2).getContent());
     }
-  
+
     @Test
-    public void update_test(){
-        //given
-        int id = 2;      
-        //when
+    public void update_test() {
+        // given
+        int id = 2;
+        // when
         Board board = boardRepository.findById(id).get();
         board.setTitle("title-update");
         board.setContent("Update-test");
-        em.flush();     
-        //eye
+        em.flush();
+        // eye
         Board result = boardRepository.findById(id).get();
         System.out.println("=======================");
         System.out.println("Board title : " + result.getTitle());
@@ -77,73 +77,76 @@ public class BoardRepositoryTest {
     }
 
     @Test
-    public void delete_test(){
-        //given
+    public void delete_test() {
+        // given
         int id = 2;
         Board board = boardRepository.findById(id).get();
-        //when
+        // when
         boardRepository.delete(board);
-        //eye
+        em.flush();
+        // eye
         List<Board> boards = boardRepository.findAll();
         System.out.println("=======================");
-        System.out.println("Board count : " + boards.size());  
-      }
+        System.out.println("Board count : " + boards.size());
+    }
 
     @Test
-    public void findByIdEager_test(){
-        //given
+    public void findByIdEager_test() {
+        // given
         int id = 1;
-        //when
+        // when
         Board board = boardRepository.findById(id).get();
-        //eye
+        // eye
         System.out.println("=======================");
         System.out.println("Board ID : " + board.getId());
     }
 
     @Test
-    public void findByIdLazy_test(){
-        //given
+    public void findByIdLazy_test() {
+        // given
         int id = 1;
-        //when
+        // when
         Board board = boardRepository.findById(id).get();
-        //eye
+        // eye
         System.out.println("=======================");
         System.out.println("Board ID : " + board.getId());
     }
 
     @Test
-    public void findByIdLazyLoading_test(){
-        //given
+    public void findByIdLazyLoading_test() {
+        // given
         int id = 1;
-        //when
+        // when
         Board board = boardRepository.findById(id).get();
-        //eye
+        // eye
         System.out.println("=======================");
         System.out.println("Board ID : " + board.getId());
         System.out.println("=======================");
         System.out.println("username : " + board.getUser().getUsername());
     }
+
     @Test
-    public void findByIdJoinUser_test(){
-        //given
+    public void findByIdJoinUser_test() {
+        // given
         int id = 1;
-        //when
+        // when
         Board board = boardRepository.findByIdJoinUser(id).get();
-        //eye
+        // eye
         System.out.println("=======================");
         System.out.println("Board ID : " + board.getId());
         System.out.println("username : " + board.getUser().getUsername());
     }
+
     @Test
-    public void findByIdJoinUserAndReply_test(){
-        //given
+    public void findByIdJoinUserAndReply_test() {
+        // given
         int id = 1;
-        //when
+        // when
         Board board = boardRepository.findByIdJoinUserAndReply(id).get();
-        //eye
+        // eye
         System.out.println("=======================");
         System.out.println("Board ID : " + board.getId());
         System.out.println("username : " + board.getUser().getUsername());
         System.out.println("Reply : " + board.getReplies().get(1).getComment());
-   }
+    }
 }
